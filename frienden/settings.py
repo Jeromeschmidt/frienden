4bd3780eca6b1a354bdc276b2237c7c9c72eede9
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-12-13 13:35:29 -0800

    heroku fix

--- frienden/settings.py
+++ frienden/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
  os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # wiki app settings
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
